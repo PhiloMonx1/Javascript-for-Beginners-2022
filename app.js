ab34d29f6610ed5793a6d2ccdb77a0d5fb93696f
2m <philomonx1@gmail.com>
2022-04-29 14:48:41 +0900

    0.3

--- app.js
+++ app.js
@@ -5,22 +5,23 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function onLoginSubmit(event){
+function onLoginSubmit(event){ //SupreFunction
     const username = loginInput.value;
     event.preventDefault();
     localStorage.setItem (USERNAME_KEY, username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    painGreetings(username);
+}
+function painGreetings(username){ //show the greeting Function
     greeting.innerText = `Hello, ${username}!!`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem (USERNAME_KEY);
 
-if (savedUsername === null){
-    // show the form
+if (savedUsername === null){ //show the form
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
-} else { // show the greeting
-    greeting.innerText = `Hello, ${savedUsername}!!`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+} else { //show the greeting
+    painGreetings(savedUsername);
 }
