2f5ef4e78d3d7404e9f7355424541ee5904d64fb
2m <philomonx1@gmail.com>
2022-05-04 08:56:01 +0900

    0.92

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,8 @@
 function onGeoOk(position){
-    console.log(position);
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("you live it");
+    alert(lat +",,,," + lng)
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.");
