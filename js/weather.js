df58c5d317c3e7be6a2f7afe96cc3d368cbbe4d9
2m <philomonx1@gmail.com>
2022-05-04 12:20:50 +0900

    0.95

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,15 @@
+const API_KIY = "7cc8ea65903c70b222adfef5ce97e148";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("you live it");
-    alert(lat +",,,," + lng)
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KIY}&units=metric`;
+    fetch(url).then(response => response.json().then(data => {
+        const weather = document.querySelector("#weather span:nth-child(1)")
+        const city = document.querySelector("#weather span:nth-child(2)")
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    }));
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.");
